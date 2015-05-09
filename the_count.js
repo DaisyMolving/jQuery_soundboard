34caed0b73ad89f0ae48bbdad59ead7d44337baf
Daisy Molving <daisymolving@gmail.com>
2015-05-10 04:01:07 +1200

    created html file with buttons and linked to jQuery, css stylesheet, js and soundmanager. added event listeners to  buttons to log click when clicked, added background image to css

---
+++ the_count.js
@@ -0,0 +1,69 @@
+// soundManager.onload = function(){
+//   soundManager.createsound()
+// }
+
+$('#countBlocks').on("click", function(){
+  console.log("click");
+//   var output =  + $(this).attr('id');
+//   $('p').text(output);
+});
+
+$('#One').on("click", function(){
+  console.log("click");
+//   var output =  + $(this).attr('id');
+//   $('p').text(output);
+});
+
+$('#Two').on("click", function(){
+  console.log("click");
+//   var output =  + $(this).attr('id');
+//   $('p').text(output);
+});
+
+$('#Three').on("click", function(){
+  console.log("click");
+//   var output =  + $(this).attr('id');
+//   $('p').text(output);
+});
+
+$('#Four').on("click", function(){
+  console.log("click");
+//   var output =  + $(this).attr('id');
+//   $('p').text(output);
+});
+
+$('#Five').on("click", function(){
+  console.log("click");
+//   var output =  + $(this).attr('id');
+//   $('p').text(output);
+});
+
+$('#Six').on("click", function(){
+  console.log("click");
+//   var output =  + $(this).attr('id');
+//   $('p').text(output);
+});
+
+$('#Seven').on("click", function(){
+  console.log("click");
+//   var output =  + $(this).attr('id');
+//   $('p').text(output);
+});
+
+$('#Eight').on("click", function(){
+  console.log("click");
+//   var output =  + $(this).attr('id');
+//   $('p').text(output);
+});
+
+$('#Nine').on("click", function(){
+  console.log("click");
+//   var output =  + $(this).attr('id');
+//   $('p').text(output);
+});
+
+$('#Ten').on("click", function(){
+  console.log("click");
+//   var output =  + $(this).attr('id');
+//   $('p').text(output);
+});
